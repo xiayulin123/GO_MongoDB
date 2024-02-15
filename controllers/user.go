a32e3c8793a52001f090e2511104e6f4c82d2886
xiayulin123 <xiayulinsky@gmail.com>
2024-02-15 16:48:57 -0500

    added 2 controllers

--- controllers/user.go
+++ controllers/user.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
+	"github.com/xiayulin123/GO_MongoDB/models"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"net/http"
 )
 
 type UserController struct{
@@ -16,4 +18,48 @@ type UserController struct{
 
 func NewUserController(s *mgo.Session) *UserController{
 	return &UserController{s}
+}
+
+//struct method
+func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	if err := uc.session.DB("mongo-go").C("users").FindId(oid).One(&u); err != nil{
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+	}
+
+func (uc UserController) CreateUser (w http.ResponseWriter, r *http.Request, _ httprouter.Param){
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-go").C("user").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
 }
